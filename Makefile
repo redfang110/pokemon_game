b18a2cbf1dd7965fdbda2f6919fe9208b135caa7
redfang110 <r3dfang110@gmail.com>
2023-03-05 15:13:39 -0600

    swapped out files for week 9

--- Makefile
+++ Makefile
@@ -1,14 +1,44 @@
-all: world
+CC = gcc
+CXX = g++
+ECHO = echo
+RM = rm -f
 
-world: generate_world.c
-	gcc -Wall -Werror -g generate_world.c -o world
+TERM = "F2022"
 
-tar:
-	cd ..
-	tar cvfz hall_noah.assignment-1.02.tar.gz hall_noah.assignment-1.02
+CFLAGS = -Wall -Werror -ggdb -funroll-loops -DTERM=$(TERM)
+CXXFLAGS = -Wall -Werror -ggdb -funroll-loops -DTERM=$(TERM)
 
-run:
-	./world
+LDFLAGS = -lncurses
+
+BIN = poke327
+OBJS = poke327.o heap.o character.o io.o db_parse.o pokemon.o
+
+all: $(BIN) etags
+
+$(BIN): $(OBJS)
+	@$(ECHO) Linking $@
+	@$(CXX) $^ -o $@ $(LDFLAGS)
+
+-include $(OBJS:.o=.d)
+
+%.o: %.c
+	@$(ECHO) Compiling $<
+	@$(CC) $(CFLAGS) -MMD -MF $*.d -c $<
+
+%.o: %.cpp
+	@$(ECHO) Compiling $<
+	@$(CXX) $(CXXFLAGS) -MMD -MF $*.d -c $<
+
+.PHONY: all clean clobber etags
 
 clean:
-	rm -f world
+	@$(ECHO) Removing all generated files
+	@$(RM) *.o $(BIN) *.d TAGS core vgcore.* gmon.out
+
+clobber: clean
+	@$(ECHO) Removing backup files
+	@$(RM) *~ \#* *pgm
+
+etags:
+	@$(ECHO) Updating TAGS
+	@etags *.[ch]
---
+++ poke327.h
@@ -0,0 +1,148 @@
+#ifndef POKE327_H
+# define POKE327_H
+
+# include <stdlib.h>
+# include <assert.h>
+
+# include "heap.h"
+
+# include "pair.h"
+
+/* Returns true if random float in [0,1] is less than *
+ * numerator/denominator.  Uses only integer math.    */
+# define rand_under(numerator, denominator) \
+  (rand() < ((RAND_MAX / denominator) * numerator))
+
+/* Returns random integer in [min, max]. */
+# define rand_range(min, max) ((rand() % (((max) + 1) - (min))) + (min))
+
+# define UNUSED(f) ((void) f)
+
+#define MAP_X              80
+#define MAP_Y              21
+#define MIN_TREES          10
+#define MIN_BOULDERS       10
+#define TREE_PROB          95
+#define BOULDER_PROB       95
+#define WORLD_SIZE         401
+#define MIN_TRAINERS       7   
+#define ADD_TRAINER_PROB   50
+#define ENCOUNTER_PROB     10
+
+#define mappair(pair) (m->map[pair[dim_y]][pair[dim_x]])
+#define mapxy(x, y) (m->map[y][x])
+#define heightpair(pair) (m->height[pair[dim_y]][pair[dim_x]])
+#define heightxy(x, y) (m->height[y][x])
+
+typedef enum __attribute__ ((__packed__)) terrain_type {
+  ter_boulder,
+  ter_tree,
+  ter_path,
+  ter_mart,
+  ter_center,
+  ter_grass,
+  ter_clearing,
+  ter_mountain,
+  ter_forest,
+  ter_exit,
+  num_terrain_types
+} terrain_type_t;
+
+typedef enum __attribute__ ((__packed__)) movement_type {
+  move_hiker,
+  move_rival,
+  move_pace,
+  move_wander,
+  move_sentry,
+  move_walk,
+  move_pc,
+  num_movement_types
+} movement_type_t;
+
+typedef enum __attribute__ ((__packed__)) character_type {
+  char_pc,
+  char_hiker,
+  char_rival,
+  char_other,
+  num_character_types
+} character_type_t;
+
+class character {
+ public:
+  virtual ~character() {};
+
+  pair_t pos;
+  char symbol;
+  int next_turn;
+};
+
+class npc : public character {
+ public:
+  character_type_t ctype;
+  movement_type_t mtype;
+  int defeated;
+  pair_t dir;
+};
+
+class pc : public character {
+};
+
+/* character is defined in poke327.h to allow an instance of character
+ * in world without including character.h in poke327.h                 */
+
+int32_t cmp_char_turns(const void *key, const void *with);
+void delete_character(void *v);
+
+int pc_move(char);
+
+extern const char *char_type_name[num_character_types];
+
+extern int32_t move_cost[num_character_types][num_terrain_types];
+
+typedef struct map {
+  terrain_type_t map[MAP_Y][MAP_X];
+  uint8_t height[MAP_Y][MAP_X];
+  character *cmap[MAP_Y][MAP_X];
+  heap_t turn;
+  int32_t num_trainers;
+  int8_t n, s, e, w;
+} map_t;
+
+void pathfind(map_t *m);
+extern void (*move_func[num_movement_types])(character *, pair_t);
+
+typedef struct world {
+  map_t *world[WORLD_SIZE][WORLD_SIZE];
+  pair_t cur_idx;
+  map_t *cur_map;
+  /* Please distance maps in world, not map, since *
+   * we only need one pair at any given time.      */
+  int hiker_dist[MAP_Y][MAP_X];
+  int rival_dist[MAP_Y][MAP_X];
+  class pc pc;
+  int quit;
+  int add_trainer_prob;
+} world_t;
+
+/* Even unallocated, a WORLD_SIZE x WORLD_SIZE array of pointers is a very *
+ * large thing to put on the stack.  To avoid that, world is a global.     */
+extern world_t world;
+
+extern pair_t all_dirs[8];
+
+#define rand_dir(dir) {     \
+  int _i = rand() & 0x7;    \
+  dir[0] = all_dirs[_i][0]; \
+  dir[1] = all_dirs[_i][1]; \
+}
+
+typedef struct path {
+  heap_node_t *hn;
+  uint8_t pos[2];
+  uint8_t from[2];
+  int32_t cost;
+} path_t;
+
+int new_map(int teleport);
+
+#endif
