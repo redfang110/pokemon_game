3d470f22ef2ae0dda4bbbed209d110b5e60b4ea9
redfang110 <r3dfang110@gmail.com>
2023-03-05 15:12:14 -0600

    swapped out files for week 8

---
+++ poke327.h
@@ -0,0 +1,294 @@
+#ifndef POKE327_H
+# define POKE327_H
+
+#include <stdlib.h>
+#include <assert.h>
+#include <string>
+#include <vector>
+
+#include "heap.h"
+
+#include "pair.h"
+
+/* Returns true if random float in [0,1] is less than *
+ * numerator/denominator.  Uses only integer math.    */
+# define rand_under(numerator, denominator) \
+  (rand() < ((RAND_MAX / denominator) * numerator))
+
+/* Returns random integer in [min, max]. */
+# define rand_range(min, max) ((rand() % (((max) + 1) - (min))) + (min))
+
+# define UNUSED(f) ((void) f)
+
+#define MAP_X              80
+#define MAP_Y              21
+#define MIN_TREES          10
+#define MIN_BOULDERS       10
+#define TREE_PROB          95
+#define BOULDER_PROB       95
+#define WORLD_SIZE         401
+#define MIN_TRAINERS       7   
+#define ADD_TRAINER_PROB   50
+
+#define mappair(pair) (m->map[pair[dim_y]][pair[dim_x]])
+#define mapxy(x, y) (m->map[y][x])
+#define heightpair(pair) (m->height[pair[dim_y]][pair[dim_x]])
+#define heightxy(x, y) (m->height[y][x])
+
+typedef enum __attribute__ ((__packed__)) terrain_type {
+  ter_boulder,
+  ter_tree,
+  ter_path,
+  ter_mart,
+  ter_center,
+  ter_grass,
+  ter_clearing,
+  ter_mountain,
+  ter_forest,
+  ter_exit,
+  num_terrain_types
+} terrain_type_t;
+
+typedef enum __attribute__ ((__packed__)) movement_type {
+  move_hiker,
+  move_rival,
+  move_pace,
+  move_wander,
+  move_sentry,
+  move_walk,
+  move_pc,
+  num_movement_types
+} movement_type_t;
+
+typedef enum __attribute__ ((__packed__)) character_type {
+  char_pc,
+  char_hiker,
+  char_rival,
+  char_other,
+  num_character_types
+} character_type_t;
+
+class character {
+ public:
+  virtual ~character() {};
+
+  pair_t pos;
+  char symbol;
+  int next_turn;
+};
+
+class npc : public character {
+ public:
+  character_type_t ctype;
+  movement_type_t mtype;
+  int defeated;
+  pair_t dir;
+};
+
+class pc : public character {
+};
+
+/* character is defined in poke327.h to allow an instance of character
+ * in world without including character.h in poke327.h                 */
+
+int32_t cmp_char_turns(const void *key, const void *with);
+void delete_character(void *v);
+
+int pc_move(char);
+
+extern const char *char_type_name[num_character_types];
+
+extern int32_t move_cost[num_character_types][num_terrain_types];
+
+typedef struct map {
+  terrain_type_t map[MAP_Y][MAP_X];
+  uint8_t height[MAP_Y][MAP_X];
+  character *cmap[MAP_Y][MAP_X];
+  heap_t turn;
+  int32_t num_trainers;
+  int8_t n, s, e, w;
+} map_t;
+
+void pathfind(map_t *m);
+extern void (*move_func[num_movement_types])(character *, pair_t);
+
+typedef struct world {
+  map_t *world[WORLD_SIZE][WORLD_SIZE];
+  pair_t cur_idx;
+  map_t *cur_map;
+  /* Please distance maps in world, not map, since *
+   * we only need one pair at any given time.      */
+  int hiker_dist[MAP_Y][MAP_X];
+  int rival_dist[MAP_Y][MAP_X];
+  class pc pc;
+  int quit;
+  int add_trainer_prob;
+} world_t;
+
+/* Even unallocated, a WORLD_SIZE x WORLD_SIZE array of pointers is a very *
+ * large thing to put on the stack.  To avoid that, world is a global.     */
+extern world_t world;
+
+extern pair_t all_dirs[8];
+
+#define rand_dir(dir) {     \
+  int _i = rand() & 0x7;    \
+  dir[0] = all_dirs[_i][0]; \
+  dir[1] = all_dirs[_i][1]; \
+}
+
+typedef struct path {
+  heap_node_t *hn;
+  uint8_t pos[2];
+  uint8_t from[2];
+  int32_t cost;
+} path_t;
+
+int new_map(int teleport);
+
+struct pokedex_pokemon {
+  int id;
+  std::string identifier;
+  int species_id;
+  int height;
+  int weight;
+  int base_experience;
+  int order;
+  int is_default;
+};
+
+struct pokedex_poke_moves {
+  int pokemon_id;
+  int version_group_id;
+  int move_id;
+  int pokemon_move_method_id;
+  int level;
+  int order;
+};
+
+struct pokedex_poke_species {
+  int id;
+  std::string identifier;
+  int generation_id;
+  int evolves_from_species_id;
+  int evolution_chain_id;
+  int color_id;
+  int shape_id;
+  int habitat_id;
+  int gender_rate;
+  int capture_rate;
+  int base_happiness;
+  int is_baby;
+  int hatch_counter;
+  int has_gender_differences;
+  int growth_rate_id;
+  int forms_switchable;
+  int is_legendary;
+  int is_mythical;
+  int order;
+  int conquest_order;
+};
+
+struct pokedex_poke_stats {
+  int pokemon_id;
+  int stat_id;
+  int base_stat;
+  int effort;
+};
+
+struct pokedex_poke_types {
+  int pokemon_id;
+  int type_id;
+  int slot;
+};
+
+struct pokedex_experience {
+  int growth_rate_id;
+  int level;
+  int experience;
+};
+
+struct pokedex_type_names {
+  int type_id;
+  int local_language_id;
+  std::string name;
+};
+
+struct pokedex_moves {
+  int id;
+  std::string identifier;
+  int generation_id;
+  int type_id;
+  int power;
+  int pp;
+  int accuracy;
+  int priority;
+  int target_id;
+  int damage_class_id;
+  int effect_id;
+  int effect_chance;
+  int contest_type_id;
+  int contest_effect_id;
+  int super_contest_effect_id;
+};
+
+struct pokedex_stats {
+  int id;
+  int damage_class_id;
+  std::string identifier;
+  int is_battle_only;
+  int game_index;
+};
+
+enum pokemon_gender {
+  male,
+  female
+};
+
+struct pokemon_stats {
+  int HP; 
+  int attack;
+  int defense; 
+  int speed; 
+  int special_attack;
+  int special_defense;
+};
+
+enum statList {
+  HP = 0, 
+  attack,
+  defense, 
+  special_attack,
+  special_defense,
+  speed,
+};
+
+class Pokemon {
+  public:
+    Pokemon(int level);
+    int id;
+    std::string identifier;
+    int species_id;
+    int base_experience;
+    bool is_shiny;
+    // int order;
+    // int is_default;
+    int level;
+    std::vector<pokedex_moves> movesList;
+    pokemon_gender gender;
+    pokemon_stats base_stats;
+    pokemon_stats IVs;
+    pokemon_stats stats;
+};
+
+extern pokedex_pokemon pokemon_list[1093];
+extern pokedex_poke_moves poke_moves[528239];
+extern pokedex_poke_species poke_species[899];
+extern pokedex_poke_stats poke_stats[6553];
+extern pokedex_poke_types poke_types[1676];
+extern pokedex_experience experience[601];
+extern pokedex_type_names type_names[19];
+extern pokedex_moves moves[845];
+extern pokedex_stats stats[9];
+
+#endif
